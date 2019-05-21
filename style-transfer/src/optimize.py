a85c36ff32d824383884dd99b7bfcdab447f63f2
Papillon6814 <so1rou.k@gmail.com>
2019-05-21 10:28:22 +0900

    ;ko

--- style-transfer/src/optimize.py
+++ style-transfer/src/optimize.py
@@ -1,162 +1,184 @@
 @@ -1,224 +0,0 @@
 from __future__ import print_function
-#python3系の機能からprint関数を持ってくる
+# python3系の機能からprint関数を持ってくる
 import functools
-#高階関数のためのモジュールfunctoolsをimportする
+# 高階関数のためのモジュールfunctoolsをimportする
 import vgg, pdb, time
-#vgg.pyをimportする
-#デバッグ用のライブラリpdbをimportする
-#時間の概念を扱うためんおライブラリtimeをimportする
+# vgg.pyをimportする
+# デバッグ用のライブラリpdbをimportする
+# 時間の概念を扱うためんおライブラリtimeをimportする
 import tensorflow as tf, numpy as np, os
-#tensorflowをimportする
-#行列演算用のライブラリnumpyをimportする
-#ファイルやディレクトリ操作用のライブラリosをimportする
+# tensorflowをimportする
+# 行列演算用のライブラリnumpyをimportする
+# ファイルやディレクトリ操作用のライブラリosをimportする
 import transform
-#transform.pyをimportする
+# transform.pyをimportする
 from utils import get_img
-#utils.pyからget_img関数をimportする
+# utils.pyからget_img関数をimportする
 
 STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
-#NNで使うためのレイヤの名前
+# NNで使うためのレイヤの名前
 CONTENT_LAYER = 'relu4_2'
 #
 DEVICES = 'CUDA_VISIBLE_DEVICES'
-#GPUの設定を読み込むための変数
+# GPUの設定を読み込むための変数
 
 # np arr, np arr
-#content_targetsはコンテンツ画像
-#style_targetはスタイル画像
-#content_weightはコンテンツ画像の損失の重み
-#style_weightはスタイル画像の損失の重み
-#tv_weightは
-#vgg_pathはvggへのパス
-#print_iterationsはチェックポイントに到達するまでの数
-#batch_sizeはバッチサイズ
-#save_pathはチェックポイントのファイルを保存するためのディレクトリのパス
-#slowはデバッグ用の機能"slow"を利用するかを指定するための引数
-#learning_rateは学習率
-#debugはデバッグモードがONかどうかを指定する引数
+# content_targetsはコンテンツ画像
+# style_targetはスタイル画像
+# content_weightはコンテンツ画像の損失の重み
+# style_weightはスタイル画像の損失の重み
+# tv_weightは
+# vgg_pathはvggへのパス
+# print_iterationsはチェックポイントに到達するまでの数
+# batch_sizeはバッチサイズ
+# save_pathはチェックポイントのファイルを保存するためのディレクトリのパス
+# slowはデバッグ用の機能"slow"を利用するかを指定するための引数
+# learning_rateは学習率
+# debugはデバッグモードがONかどうかを指定する引数
 def optimize(content_targets, style_target, content_weight, style_weight,
              tv_weight, vgg_path, epochs=2, print_iterations=1000,
              batch_size=4, save_path='saver/fns.ckpt', slow=False,
              learning_rate=1e-3, debug=False):
-    #パラメータの最適化をするための関数optimizer()
+    # パラメータの最適化をするための関数optimizer()
     if slow:
-        #slowが指定されていればTrue
+        # slowが指定されていればTrue
         batch_size = 1
-        #バッチサイズを１と指定する
+        # バッチサイズを１と指定する
     mod = len(content_targets) % batch_size
-    #コンテンツ画像を効率良くバッチサイズに割り当てて学習できるかを見ておく
-    #content_targetsとバッチサイズの剰余をmodに格納する
+    # コンテンツ画像を効率良くバッチサイズに割り当てて学習できるかを見ておく
+    # content_targetsとバッチサイズの剰余をmodに格納する
     if mod > 0:
-        #modが0より大きければTrue
+        # modが0より大きければTrue
         print("Train set has been trimmed slightly..")
-        #訓練用の画像を少しだけ削減して使うことをコンソールに書き込む
+        # 訓練用の画像を少しだけ削減して使うことをコンソールに書き込む
         content_targets = content_targets[:-mod]
-        #content_targetsに格納されている画像セットから、剰余の分を後ろから削減する
+        # content_targetsに格納されている画像セットから、剰余の分を後ろから削減する
 
     style_features = {}
-    #style_features変数を初期化する
+    # style_features変数を初期化する
 
     batch_shape = (batch_size,256,256,3)
-    #バッチサイズを考慮して4次元のテンソルを作り、batch_shape変数に格納する
+    # バッチサイズを考慮して4次元のテンソルを作り、batch_shape変数に格納する
     style_shape = (1,) + style_target.shape
-    #スタイル画像にもう一次元追加して4次元のテンソルを作り、style_shape変数に格納する
+    # スタイル画像にもう一次元追加して4次元のテンソルを作り、style_shape変数に格納する
 
     print(style_shape)
-    #スタイル画像の形をコンソールに出力する
+    # スタイル画像の形をコンソールに出力する
 
     # precompute style features
     with tf.Graph().as_default(), tf.device('/cpu:0'), tf.Session() as sess:
-        #tensorflowのグラフを作成し、デバイスをcpuに設定し、tf.Session()でグラフの実行環境を独立させる
+        # tensorflowのグラフを作成し、デバイスをcpuに設定し、tf.Session()でグラフの実行環境を独立させる
         style_image = tf.placeholder(tf.float32, shape=style_shape, name='style_image')
-        #入力用のノードをplaceholderを使って作成する
+        # 入力用のノードをplaceholderを使って作成する
         style_image_pre = vgg.preprocess(style_image)
-        #style_imageに格納されている画像をpreprocess関数を用いて前処理を行う
+        # style_imageに格納されている画像をpreprocess関数を用いて前処理を行う
         net = vgg.net(vgg_path, style_image_pre)
-        #定義したCNNをnet変数に格納する
+        # 定義したCNNをnet変数に格納する
         style_pre = np.array([style_target])
-        #スタイル画像を行列に変換する
+        # スタイル画像を行列に変換する
         for layer in STYLE_LAYERS:
-            #STYLE_LAYERS変数に格納されている文字列をlayer変数に格納していく
+            # STYLE_LAYERS変数に格納されている文字列をlayer変数に格納していく
             features = net[layer].eval(feed_dict={style_image:style_pre})
-            #layer変数の中身をひとつの要素としてnet変数にスタイル画像の行列をfeed_dictとして与え、
-            #対応するNNを実行して結果をfeaturesに格納する
+            # layer変数の中身をひとつの要素としてnet変数にスタイル画像の行列をfeed_dictとして与え、
+            # 対応するNNを実行して結果をfeaturesに格納する
             features = np.reshape(features, (-1, features.shape[3]))
-            #features変数を[features[3], -1]に成形する
+            # features変数を[features[3], -1]に成形する
             gram = np.matmul(features.T, features) / features.size
-            #features行列の転置行列とオリジナルの行列をmatmulで乗算し、
-            #その結果取得した値をfeaturesの要素数で除算し、gram変数に格納する
+            # features行列の転置行列とオリジナルの行列をmatmulで乗算し、
+            # その結果取得した値をfeaturesの要素数で除算し、gram変数に格納する
+            # ここでグラム行列を取得できる
             style_features[layer] = gram
-            #style_featuresリストのlayer(ループ毎に変わる)要素のものにgram変数の中身を格納しておく
-            #なんでかはしらん
+            # style_featuresリストのlayer(ループ毎に変わる)要素のものにgram変数の中身を格納しておく
+            # これはスタイル元画像のグラム行列
 
     with tf.Graph().as_default(), tf.Session() as sess:
-        #tensorflowのグラフを作成し、セッションを開始して独立空間を作り出す
+        # tensorflowのグラフを作成し、セッションを開始して独立空間を作り出す
         X_content = tf.placeholder(tf.float32, shape=batch_shape, name="X_content")
-        #入力用のノードをplaceholderを使って作成し、名前はX_content、サイズはbatch_shapeと同じようにしておく
+        # 入力用のノードをplaceholderを使って作成し、名前はX_content、サイズはbatch_shapeと同じようにしておく
         X_pre = vgg.preprocess(X_content)
-        #X_contentの入力ノードに入力されるのは基本的に画像なので、preprocess関数を使って前処理を行い、
-        #その結果をX_pre変数に格納する
+        # X_contentの入力ノードに入力されるのは基本的に画像なので、preprocess関数を使って前処理を行い、
+        # その結果をX_pre変数に格納する
 
         # precompute content features
         content_features = {}
-        #content_features変数を定義する
+        # content_features変数を定義する
         content_net = vgg.net(vgg_path, X_pre)
-        #content_net変数に、X_preをVGGのCNNに通した結果を格納する
+        # content_net変数に、X_preをVGGのCNNに通した結果を格納する
         content_features[CONTENT_LAYER] = content_net[CONTENT_LAYER]
-        #content_features変数の、'relu4_2'の要素のところにCNNを通して出力された値の'relu4_2'の結果を格納
-        #つまり、この変数に格納されている値もnetである（tensor）
+        # content_features変数の、'relu4_2'の要素のところにCNNを通して出力された値の'relu4_2'の結果を格納
+        # つまり、この変数に格納されている値もnetである（tensor）
 
         if slow:
-            #slowモードのとき
+            # slowモードのとき
             preds = tf.Variable(
                 tf.random_normal(X_content.get_shape()) * 0.256
-                #入力ノードの形で正規分布で初期化をし、値に0.256をかける
+                # 入力ノードの形で正規分布で初期化をし、値に0.256をかける
             )
-            #そしてその値（行列）をpredsに格納する
+            # そしてその値（行列）をpredsに格納する
             preds_pre = preds
-            #preds_pre変数にpredsを格納する
+            # preds_pre変数にpredsを格納する
         else:
-            #slowモードでないとき
+            # slowモードでないとき
             preds = transform.net(X_content/255.0)
-            #入力ノードの値を255で割ってマッピングのネットワークを通す
+            # 入力ノードの値を255で割ってマッピングのネットワークを通す
             preds_pre = vgg.preprocess(preds)
-            #preds変数の中の値を画像として前処理を行う
+            # preds変数の中の値を画像として前処理を行う
 
         net = vgg.net(vgg_path, preds_pre)
-        #preds_preをvggの方にあるCNNに通し、VGGの畳み込み層（合成のネットワーク）を通す
+        # preds_preをvggの方にあるCNNに通し、VGGの畳み込み層（合成のネットワーク）を通す
 
         content_size = _tensor_size(content_features[CONTENT_LAYER])*batch_size
-        #_tensor_size関数を使って取得したテンソルの形（サイズ）にbatch_sizeを掛け合わせ、
-        #それをcontent_sizeとして変数に格納する
+        # _tensor_size関数を使って取得したテンソルの形（サイズ）にbatch_sizeを掛け合わせ、
+        # それをcontent_sizeとして変数に格納する
         assert _tensor_size(content_features[CONTENT_LAYER]) == _tensor_size(net[CONTENT_LAYER])
-        #content_featuresのCONTENT_LAYERの中身とnetのCONTENT_LAYERの中身が同じでなければAssertionError
-        #同じ層を動作させているかを確認するためと思われる
+        # content_featuresのCONTENT_LAYERの中身とnetのCONTENT_LAYERの中身が同じでなければAssertionError
+        # スタイル用のNNとコンテンツ用のNNが同じ大きさであることを確認するためと思われる
         content_loss = content_weight * (2 * tf.nn.l2_loss(
-            #l2_loss()関数は二乗誤差を求めるための関数
+            # l2_loss()関数は二乗誤差を求めるための関数
             net[CONTENT_LAYER] - content_features[CONTENT_LAYER]) / content_size
-            #net変数の方は実際に機械がマッピングして算出した実績値を保持している
-            #content_features変数の方は入力された画像をCNNに通したときの予測値を保持している
-            #上の２つの数を減算してcontent_size変数に格納されているテンソルのサイズで除算し、
-            #それをコンテンツの損失の値として利用する
+            # net変数の方は実際に機械がマッピングして算出した実績値を保持している
+            # content_features変数の方は入力された画像をCNNに通したときの予測値を保持している
+            # 上の２つの数を減算してcontent_size変数に格納されているテンソルのサイズで除算し、
+            # コンテンツの損失を求めるための平均二乗和誤差をここで求めている
         )
 
         style_losses = []
+        # スタイル損失を格納しておくための変数style_lossesを定義
         for style_layer in STYLE_LAYERS:
+            # レイヤの名前が格納されている変数STYLE_LAYERSの分だけループする
             layer = net[style_layer]
+            # layer変数に現在のレイヤを格納する
             bs, height, width, filters = map(lambda i:i.value,layer.get_shape())
+            # 高階関数map()を使って、現在のレイヤ（ネットワーク）の形を取得する
+            # bsはバッチサイズ
+            # heightはレイヤの縦の大きさ
+            # widthはレイヤの横の大きさ
+            # filtersはレイヤの深さ？
             size = height * width * filters
+            # 取得した数値のうちのbs以外を用いて特徴マップの体積を求める
             feats = tf.reshape(layer, (bs, height * width, filters))
+            # 現在のレイヤの形を[バッチサイズ 特徴マップのサイズ 特徴マップの深さ（数）]に変更
+            # そしてfeats変数に格納する
             feats_T = tf.transpose(feats, perm=[0,2,1])
+            # feats変数の転置行列をfeats_T変数に格納する
             grams = tf.matmul(feats_T, feats) / size
+            # feats_T * feats をしてfeatsのグラム行列を取得し、それをsizeで割って
+            # gram変数に格納する
+            # これは特徴マップのグラム行列
             style_gram = style_features[style_layer]
+            # スタイル元画像のグラム行列をstyle_gram変数に格納する
             style_losses.append(2 * tf.nn.l2_loss(grams - style_gram)/style_gram.size)
+            # style_losses変数に、特徴マップのグラム行列とスタイル元画像の差分をとり、
+            # その平均二乗和誤差を格納する
 
         style_loss = style_weight * functools.reduce(tf.add, style_losses) / batch_size
+        # 層ごとの平均二乗和誤差の和の値と、スタイル損失の重みの値を掛け算し、batch_sizeで割ることで
+        # スタイル損失の合計損失を求めることができる
 
         # total variation denoising
         tv_y_size = _tensor_size(preds[:,1:,:,:])
+        #
         tv_x_size = _tensor_size(preds[:,:,1:,:])
         y_tv = tf.nn.l2_loss(preds[:,1:,:,:] - preds[:,:batch_shape[1]-1,:,:])
         x_tv = tf.nn.l2_loss(preds[:,:,1:,:] - preds[:,:,:batch_shape[2]-1,:])
--- style-transfer/src/vgg.py
+++ style-transfer/src/vgg.py
@@ -1,23 +1,23 @@
 # Copyright (c) 2015-2016 Anish Athalye. Released under GPLv3.
 
 import tensorflow as tf
-#tensorflowをimportする
+# tensorflowをimportする
 import numpy as np
-#行列演算用のライブラリnumpyをimportする
+# 行列演算用のライブラリnumpyをimportする
 import scipy.io
-#ファイルの入出力用のライブラリscipy.ioをimportする
+# ファイルの入出力用のライブラリscipy.ioをimportする
 import pdb
-#デバッグ用のライブラリpdbをimportする
+# デバッグ用のライブラリpdbをimportする
 
 MEAN_PIXEL = np.array([ 123.68 ,  116.779,  103.939])
-#vgg用のRGB平均値
+# vgg用のRGB平均値
 
-#data_pathは読み取りたいmatlabファイルのパス
-#input_imageは画像を格納する引数
+# data_pathは読み取りたいmatlabファイルのパス
+# input_imageは画像を格納する引数
 def net(data_path, input_image):
-    #net関数は実際に畳み込みなどの処理を行うための関数
+    # net関数は実際に畳み込みなどの処理を行うための関数
     layers = (
-        #画風変換で用いるネットワークの層を格納しておく
+        # 画風変換で用いるネットワークの層を格納しておく
         'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
 
         'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
@@ -33,83 +33,83 @@ def net(data_path, input_image):
     )
 
     data = scipy.io.loadmat(data_path)
-    #matlabファイルを読み取る関数scipy.io.loadmat()を使う
-    #data_pathに格納されているvggのパスから、matlabファイルの変数をすべて読み取る
+    # matlabファイルを読み取る関数scipy.io.loadmat()を使う
+    # data_pathに格納されているvggのパスから、matlabファイルの変数をすべて読み取る
     mean = data['normalization'][0][0][0]
-    #matlabファイルから読み取ったdata変数に格納されている、
-    #3次元のnormalizationワークスペース変数の中身をmean変数に代入する
+    # matlabファイルから読み取ったdata変数に格納されている、
+    # 3次元のnormalizationワークスペース変数の中身をmean変数に代入する
     mean_pixel = np.mean(mean, axis=(0, 1))
-    #mean_pixel変数に、正規化するための値meanを1次元の変数に行列を直して格納する
+    # mean_pixel変数に、正規化するための値meanを1次元の変数に行列を直して格納する
     weights = data['layers'][0]
-    #matlabファイルの中に書かれているlayersワークスペース変数から重みの値を取り出し、
-    #layers変数に格納する
+    # matlabファイルの中に書かれているlayersワークスペース変数から重みの値を取り出し、
+    # layers変数に格納する
 
     net = {}
-    #net変数を初期化する
+    # net変数を初期化する
     current = input_image
-    #current変数にinput_imageに格納されている画像を格納する
+    # current変数にinput_imageに格納されている画像を格納する
     for i, name in enumerate(layers):
-        #layersの中身を、enumerate関数を用いてインデックス番号付きの要素としてforループさせる
-        #この場合、iがインデックス番号でnameが層の名前（convとかreluとか）
+        # layersの中身を、enumerate関数を用いてインデックス番号付きの要素としてforループさせる
+        # この場合、iがインデックス番号でnameが層の名前（convとかreluとか）
         kind = name[:4]
-        #インデックス4つ分をnameから取り出して、kind変数に格納する
-        #conv, relu, conv, relu
+        # インデックス4つ分をnameから取り出して、kind変数に格納する
+        # conv, relu, conv, relu
         if kind == 'conv':
-            #kind変数の中身がconvである場合true
+            # kind変数の中身がconvである場合true
             kernels, bias = weights[i][0][0][0][0]
-            #kernels変数の方には実際の重みの値を格納する（たぶん）
-            #bias変数の方にはバイアスとなるベクトルが格納される
+            # kernels変数の方には実際の重みの値を格納する（たぶん）
+            # bias変数の方にはバイアスとなるベクトルが格納される
 
             # matconvnet: weights are [width, height, in_channels, out_channels]
             # tensorflow: weights are [height, width, in_channels, out_channels]
             kernels = np.transpose(kernels, (1, 0, 2, 3))
-            #kernelsに入っている行列を転置行列にしてdeconvする
+            # kernelsに入っている行列を転置行列にしてdeconvする
             bias = bias.reshape(-1)
-            #bias変数の中身を一次ベクトルに変換して再格納する
+            # bias変数の中身を一次ベクトルに変換して再格納する
             current = _conv_layer(current, kernels, bias)
-            #current変数を畳み込みレイヤを通したあとに再度格納する
+            # current変数を畳み込みレイヤを通したあとに再度格納する
         elif kind == 'relu':
-            #kind変数の中身がreluだった場合
+            # kind変数の中身がreluだった場合
             current = tf.nn.relu(current)
-            #current変数をrelu関数で活性化して、current変数に再格納する
+            # current変数をrelu関数で活性化して、current変数に再格納する
         elif kind == 'pool':
-            #kind変数の中身がpoolだった場合
+            # kind変数の中身がpoolだった場合
             current = _pool_layer(current)
-            #current変数をプーリングして再度格納する
+            # current変数をプーリングして再度格納する
         net[name] = current
-        #netリストnameの要素の場所にcurrentの中身を格納する
+        # netリストのnameの要素の場所にcurrentの中身を格納する
 
     assert len(net) == len(layers)
-    #net変数の要素の数とlayers変数の要素の数が違ったらAssertionError
+    # net変数の要素の数とlayers変数の要素の数が違ったらAssertionError
     return net
-    #net変数をreturnする
+    # net変数をreturnする
 
-#inputは畳み込むための画像
-#weightsは重みの値を格納する変数
-#biasはバイアスの値が格納されている
+# inputは畳み込むための画像
+# weightsは重みの値を格納する変数
+# biasはバイアスの値が格納されている
 def _conv_layer(input, weights, bias):
-    #input画像をweightsの重みとbiasのバイアスを加算してreturnする関数
+    # input画像をweightsの重みとbiasのバイアスを加算してreturnする関数
     conv = tf.nn.conv2d(input, tf.constant(weights), strides=(1, 1, 1, 1),
             padding='SAME')
-    #inputを畳み込み層に入れて畳み込み、conv変数に格納する
+    # inputを畳み込み層に入れて畳み込み、conv変数に格納する
     return tf.nn.bias_add(conv, bias)
-    #畳み込んだ画像convにbiasを加算してreturnする
+    # 畳み込んだ画像convにbiasを加算してreturnする
 
-#inputはプーリングするための画像
+# inputはプーリングするための画像
 def _pool_layer(input):
-    #画像をプーリングするための関数_pool_layer()
+    # 画像をプーリングするための関数_pool_layer()
     return tf.nn.max_pool(input, ksize=(1, 2, 2, 1), strides=(1, 2, 2, 1),
             padding='SAME')
-    #最大プーリングをしてreturnする
+    # 最大プーリングをしてreturnする
 
-#image変数は画像を格納するための変数
+# image変数は画像を格納するための変数
 def preprocess(image):
-    #訓練データの前処理を行うための関数preprocess()
+    # 訓練データの前処理を行うための関数preprocess()
     return image - MEAN_PIXEL
-    #画像からRGBの平均値をマイナスする
+    # 画像からRGBの平均値をマイナスする
 
-#image変数は画像を格納するための変数
+# image変数は画像を格納するための変数
 def unprocess(image):
-    #訓練データの前処理を打ち消すための変数unprocess()
+    # 訓練データの前処理を打ち消すための変数unprocess()
     return image + MEAN_PIXEL
-    #画像にRGBの平均値をプラスする
+    # 画像にRGBの平均値をプラスする
